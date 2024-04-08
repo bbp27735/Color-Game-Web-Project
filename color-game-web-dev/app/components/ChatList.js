07663afb3e651bb415e607f655f4bf38a249f567
wts83026 <83028652+wts83026@users.noreply.github.com>
2024-04-08 18:10:01 -0400

    Chat
    Added 2 new default chats and added profile pic images

--- color-game-web-dev/app/components/ChatList.js
+++ color-game-web-dev/app/components/ChatList.js
@@ -6,10 +6,23 @@ import AddChat from "./AddChat"
 
 const ChatBar = (props) => {
 
-    const listOfChats = [{
-        username: "Debug",
-        message: "Hello World!"
-    }]
+    const listOfChats = [
+        {
+            username: "Debug",
+            message: "Hello World!",
+            img: "https://i.pinimg.com/564x/f0/e2/3e/f0e23e83f07e9da8f8ded461b4284504.jpg"
+        },
+        {
+            username: "Mario",
+            message: "Wahooo",
+            img: "https://upload.wikimedia.org/wikipedia/en/a/a9/MarioNSMBUDeluxe.png"
+        },
+        {
+            username: "Stitch",
+            message: "Ohana",
+            img: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Stitch_%28Lilo_%26_Stitch%29.svg/1200px-Stitch_%28Lilo_%26_Stitch%29.svg.png"
+        }
+    ]
 
     const [chats, setChats] = useState(listOfChats);
 
@@ -33,7 +46,7 @@ const ChatBar = (props) => {
         <div>
             <ul class="chat-ul">
                 {chats.map((chat) => 
-                    <Chat key={chat.id} username={chat.username} message={chat.message} />
+                    <Chat key={chat.id} username={chat.username} message={chat.message} img={chat.img} />
                )}
             </ul>
                 <AddChat onSendChat={handleChats}/>
