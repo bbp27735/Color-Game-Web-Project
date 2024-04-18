bb7b3408100a57dafa4c189e388d1c96d4f64d44
literallytoast <115420736+literallytoast@users.noreply.github.com>
2024-04-18 16:32:13 -0400

    Created basic user model/schema & user
    Added files for User, Stat, and Message schemas. They aren't set up within the frontend yet. However, the routes for User work with Postman. Need to set the others up properly.

---
+++ color-game-server/models/Message.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+
+// we create a file for each model
+
+
+
+const ChatSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    messageContent: {
+        type: String,
+    },
+    image: {
+        type: String,
+    },
+    updated_date: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+module.exports = Message = mongoose.model('message', ChatSchema);
+// mongoose.model compiles the model, which we are referring to as Item
+// We can invoke our database methods on Item
+// First argument 'item' is the sigular name of the collection
+// So, even though our file is named items, the singular version is item, the name of the collection
+// so, we refer to our mongoose element here as Item
