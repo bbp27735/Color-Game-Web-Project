1a2645bc26934888c0e92986147dffce769540cb
wts83026 <83028652+wts83026@users.noreply.github.com>
2024-04-29 17:19:03 -0400

    Added a few things to the chats

--- color-game-frontend/app/components/ChatList.js
+++ color-game-frontend/app/components/ChatList.js
@@ -1,29 +1,43 @@
 'use client'
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import UserContext from '../context/UserContext';
 import Chat from './Chat';
 import './ChatList.css';
 import AddChat from "./AddChat"
+import axios from 'axios';
 
 const ChatList = (props) => {
+
+    const axios = require('axios');
+
+    let returnChats = [];
+
+    const getData = async () => {
+        returnChats = await axios.get('http://localhost:8084/api/chats/');
+        console.log('THIS IS MY GET REQUEST', returnChats.data);
+      }
+
+      useEffect( () => {
+        getData();
+      }, []);
 
     const listOfChats = [{
         id: 1,
         username: "Debug",
-        message: "Hello World!",
-        img: "https://i.pinimg.com/564x/f0/e2/3e/f0e23e83f07e9da8f8ded461b4284504.jpg"
+        chatContent: "Hello World!",
+        image: "https://i.pinimg.com/564x/f0/e2/3e/f0e23e83f07e9da8f8ded461b4284504.jpg"
     },
     {
         id: 2,
         username: "Mario",
-        message: "Wahooo",
-        img: "https://upload.wikimedia.org/wikipedia/en/a/a9/MarioNSMBUDeluxe.png"
+        chatContent: "Wahooo",
+        image: "https://upload.wikimedia.org/wikipedia/en/a/a9/MarioNSMBUDeluxe.png"
     },
     {
         id: 3,
         username: "Stitch",
-        message: "Ohana",
-        img: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Stitch_%28Lilo_%26_Stitch%29.svg/1200px-Stitch_%28Lilo_%26_Stitch%29.svg.png"
+        chatContent: "Ohana",
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Stitch_%28Lilo_%26_Stitch%29.svg/1200px-Stitch_%28Lilo_%26_Stitch%29.svg.png"
     }]
 
     const [chats, setChats] = useState(listOfChats);
@@ -37,22 +51,19 @@ const ChatList = (props) => {
         ...enteredChatData,
         id : Math.random().toString(),
       
-        }
+        }   
 
-        // if (chatData.username != '' && chatData.message != '') {
-        //   setChats([...chats, chatData])
-        // }        
-        
-        if (chatData.username !== '' && chatData.message !== '') {
+        if (chatData.username !== '' && chatData.chatContent !== '') {
             // Create a new array with the new chat data and the current chats
-            let updatedChats = [...chats, chatData];
+            let updatedChats = [chatData, ...chats];
     
             // Remove oldest chat if chats array length exceeds 5
             if (updatedChats.length > 5) {
-                updatedChats.shift(); // Remove the first item (oldest chat)
+                updatedChats.pop(); // Remove the first item (oldest chat)
             }
     
             setChats(updatedChats);
+
         }
 
     }
@@ -70,7 +81,7 @@ const ChatList = (props) => {
         <div className="chatDiv">
         <div className="chat-ul">
             {chats.map((chat) => 
-                <Chat key={chat.id} img ={chat.img} username={chat.username} message={chat.message} onDelete={() => handleDeleteChat(chat.id)} />
+                <Chat key={chat.id} image ={chat.image} username={chat.username} message={chat.chatContent} updatedData={chat.updated_date} onDelete={() => handleDeleteChat(chat.id)} />
             )}
                 
         </div>
